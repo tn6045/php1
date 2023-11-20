6f531c27fd5da9d9129a0c9c08c177c2d370a5ea
Mohamed Zied HOSNI <mohamed.zied@live.fr>
2023-11-20 23:57:06 +0100

    add echo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 use Carbon\Carbon;
 
-
+echo ("test msg");
 printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
 
 printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
